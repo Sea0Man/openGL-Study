22119e2d130a7d4ee020485dfdd86353e603caca
leqqe04 <leqqe04@gmail.com>
2026-05-04 12:12:26 +0400

    impelented model loading

---
+++ model/build/CMakeFiles/model.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/model.dir/src/glad.c.o"
+  "CMakeFiles/model.dir/src/glad.c.o.d"
+  "CMakeFiles/model.dir/src/main.cpp.o"
+  "CMakeFiles/model.dir/src/main.cpp.o.d"
+  "model"
+  "model.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
